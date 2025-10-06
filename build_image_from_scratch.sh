769e379151d67f52e42ed7e38bf5a9f2e4ef7648
manish1307hap <manish.mangla-c@haptik.co>
2025-10-06 22:58:27 +0530

    setupt alembic

--- build_image_from_scratch.sh
+++ build_image_from_scratch.sh
@@ -4,9 +4,6 @@ set -e  # Exit immediately if any command fails
 echo "Step 1: Building Docker image..."
 docker compose build
 
-echo "Step 2: Running Alembic migrations..."
-docker compose run --rm web alembic upgrade head
-
 echo "Step 3: Starting containers..."
 docker compose up -d
 
